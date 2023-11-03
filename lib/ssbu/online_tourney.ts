ca292576c0cb7309a6c868df9aa778c858280078
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-11-03 00:13:55 -0700

    fix utc date discrepancy

--- lib/ssbu/online_tourney.ts
+++ lib/ssbu/online_tourney.ts
@@ -26,7 +26,7 @@ export function tourneyAt(date: Date, offset = 0): Tourney {
 
 const ORIGINAL_TOURNEY: Tourney = {
   name: "1-on-1",
-  startDate: new Date(2019, 6, 31, 6, 0, 0, 0),
+  startDate: new Date(Date.UTC(2019, 6, 31, 6, 0, 0, 0)),
 };
 
 const TOURNEY_DURATION = 2 * 24 * 60 * 60 * 1_000;
