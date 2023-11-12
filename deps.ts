02e862184fda8494f9b2ed3bfc61c78701b89194
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-11-12 15:37:58 -0800

    run `deno task all`

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { assertEquals } from "https://deno.land/std@0.205.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.206.0/testing/asserts.ts";
 export { Duration } from "https://deno.land/x/durationjs@v4.1.0/mod.ts";
